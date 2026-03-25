fcce221c6eb1391cc91e485c8656e81d7ad6d0e1
jeffolsen <olsenjeffn@gmail.com>
2026-03-25 12:54:28 -0400

    add actions, tags and components

--- index.d.ts
+++ index.d.ts
@@ -4,8 +4,30 @@ interface ContentImage {
   type?: "LANDSCAPE" | "PORTRAIT" | "ICON" | "OTHER";
 }
 
-interface ContentData {
+interface ContentTag {
+  name: string;
+}
+
+interface ContentComponentType {
+  name: string;
+  itemBinding: "FILTERED_BY_TAG" | "MANUALLY_CURATED" | "BY_URL";
+}
+
+interface UpsertableContentData {
   images: ContentImage[];
+  tags: ContentTag[];
+  componentTypes: ContentComponentType[];
+}
+
+interface DeletableContentData {
+  images: string[]; // URLs of images to delete
+  tags: string[]; // Names of tags to delete
+  componentTypes: string[]; // Names of component types to delete
+}
+
+interface ContentData {
+  deletes: DeletableContentData;
+  upserts: UpsertableContentData;
 }
 
 declare const content: ContentData;
